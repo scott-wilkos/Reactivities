e7cad4be0ed1aae7901b46a67454fcc7b0ce0b34
scott-wilkos <scott@mydrugcosts.com>
2022-01-16 09:53:20 -0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,12 @@ namespace API
             services.AddDbContext<DataContext>(opt =>{
                 opt.UseSqlite(this._config.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,9 +54,11 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
